3eadc03faaab8a818a2fc02b85969895b73660a9
Pallab <pallabchatt@gmail.com>
2020-05-20 21:15:12 -0400

    Initial Commit

---
+++ src/com/pallab/util/constant/CommonConstant.java
@@ -0,0 +1,11 @@
+package com.pallab.util.constant;
+
+public class CommonConstant {
+	public static final String INR_CODE = "&#8377;";
+	public static final String SEPERATOR_CODE = "-->";
+	public static final String EMAIL_SUBJECT = "[For Your Action] - Stock Price Alart";
+	public static final String EMAIL_BODY = "Below mentioned stock price decreased below your Expectation: \n";
+	public static final String TO_EMAIL = "stockmonitor17@gmail.com";
+	public static final String CC_EMAIL= "suparna.pallab@gmail.com";
+
+}
---
+++ src/com/pallab/util/exception/StockException.java
@@ -0,0 +1,22 @@
+package com.pallab.util.exception;
+
+public class StockException extends RuntimeException {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private String message = null;
+
+	public StockException(String message) {
+		super();
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+}
